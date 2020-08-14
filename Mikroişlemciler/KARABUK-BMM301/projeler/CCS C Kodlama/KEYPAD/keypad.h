10d0663c0137f69f4eb2a04f88b25ad650604908
ahmetekmell <ekmel.147@gmail.com>
2020-08-14 22:52:21 +0300

    [mikroişlemciler > CCS_C ] mod10000 ve keypad kodları eklendi.

---
+++ Mikroişlemciler/KARABUK-BMM301/projeler/CCS C Kodlama/KEYPAD/keypad.h
@@ -0,0 +1,9 @@
+#include <16F877A.h>
+#device ADC=16
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(crystal=20000000)
+
